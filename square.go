a9c90c6a9d9101c9885830f568d26b2280e5cbe7
Cam <16303791+speedyhoon@users.noreply.github.com>
2023-12-31 19:04:46 +1100

    Move all global variables into a new Puzzle struct type.

--- square.go
+++ square.go
@@ -30,61 +30,61 @@ func whichSquare(row, col byte) byte {
 	}[row][col]
 }
 
-func (p *puzzle) Square(index byte) *group {
+func (p *Puzzle) Square(index byte) *Group {
 	switch index {
 	case topLeft:
-		return &group{
-			&p[0][0], &p[0][1], &p[0][2],
-			&p[1][0], &p[1][1], &p[1][2],
-			&p[2][0], &p[2][1], &p[2][2],
+		return &Group{
+			&p.Cells[0][0], &p.Cells[0][1], &p.Cells[0][2],
+			&p.Cells[1][0], &p.Cells[1][1], &p.Cells[1][2],
+			&p.Cells[2][0], &p.Cells[2][1], &p.Cells[2][2],
 		}
 	case topCent:
-		return &group{
-			&p[0][3], &p[0][4], &p[0][5],
-			&p[1][3], &p[1][4], &p[1][5],
-			&p[2][3], &p[2][4], &p[2][5],
+		return &Group{
+			&p.Cells[0][3], &p.Cells[0][4], &p.Cells[0][5],
+			&p.Cells[1][3], &p.Cells[1][4], &p.Cells[1][5],
+			&p.Cells[2][3], &p.Cells[2][4], &p.Cells[2][5],
 		}
 	case topRight:
-		return &group{
-			&p[0][6], &p[0][7], &p[0][8],
-			&p[1][6], &p[1][7], &p[1][8],
-			&p[2][6], &p[2][7], &p[2][8],
+		return &Group{
+			&p.Cells[0][6], &p.Cells[0][7], &p.Cells[0][8],
+			&p.Cells[1][6], &p.Cells[1][7], &p.Cells[1][8],
+			&p.Cells[2][6], &p.Cells[2][7], &p.Cells[2][8],
 		}
 	case midLeft:
-		return &group{
-			&p[3][0], &p[3][1], &p[3][2],
-			&p[4][0], &p[4][1], &p[4][2],
-			&p[5][0], &p[5][1], &p[5][2],
+		return &Group{
+			&p.Cells[3][0], &p.Cells[3][1], &p.Cells[3][2],
+			&p.Cells[4][0], &p.Cells[4][1], &p.Cells[4][2],
+			&p.Cells[5][0], &p.Cells[5][1], &p.Cells[5][2],
 		}
 	case center:
-		return &group{
-			&p[3][3], &p[3][4], &p[3][5],
-			&p[4][3], &p[4][4], &p[4][5],
-			&p[5][3], &p[5][4], &p[5][5],
+		return &Group{
+			&p.Cells[3][3], &p.Cells[3][4], &p.Cells[3][5],
+			&p.Cells[4][3], &p.Cells[4][4], &p.Cells[4][5],
+			&p.Cells[5][3], &p.Cells[5][4], &p.Cells[5][5],
 		}
 	case midRight:
-		return &group{
-			&p[3][6], &p[3][7], &p[3][8],
-			&p[4][6], &p[4][7], &p[4][8],
-			&p[5][6], &p[5][7], &p[5][8],
+		return &Group{
+			&p.Cells[3][6], &p.Cells[3][7], &p.Cells[3][8],
+			&p.Cells[4][6], &p.Cells[4][7], &p.Cells[4][8],
+			&p.Cells[5][6], &p.Cells[5][7], &p.Cells[5][8],
 		}
 	case bottomLeft:
-		return &group{
-			&p[6][0], &p[6][1], &p[6][2],
-			&p[7][0], &p[7][1], &p[7][2],
-			&p[8][0], &p[8][1], &p[8][2],
+		return &Group{
+			&p.Cells[6][0], &p.Cells[6][1], &p.Cells[6][2],
+			&p.Cells[7][0], &p.Cells[7][1], &p.Cells[7][2],
+			&p.Cells[8][0], &p.Cells[8][1], &p.Cells[8][2],
 		}
 	case bottomCent:
-		return &group{
-			&p[6][3], &p[6][4], &p[6][5],
-			&p[7][3], &p[7][4], &p[7][5],
-			&p[8][3], &p[8][4], &p[8][5],
+		return &Group{
+			&p.Cells[6][3], &p.Cells[6][4], &p.Cells[6][5],
+			&p.Cells[7][3], &p.Cells[7][4], &p.Cells[7][5],
+			&p.Cells[8][3], &p.Cells[8][4], &p.Cells[8][5],
 		}
 	case bottomRight:
-		return &group{
-			&p[6][6], &p[6][7], &p[6][8],
-			&p[7][6], &p[7][7], &p[7][8],
-			&p[8][6], &p[8][7], &p[8][8],
+		return &Group{
+			&p.Cells[6][6], &p.Cells[6][7], &p.Cells[6][8],
+			&p.Cells[7][6], &p.Cells[7][7], &p.Cells[7][8],
+			&p.Cells[8][6], &p.Cells[8][7], &p.Cells[8][8],
 		}
 	}
 
@@ -92,7 +92,7 @@ func (p *puzzle) Square(index byte) *group {
 	return nil
 }
 
-func (g *group) has(val byte) bool {
+func (g *Group) has(val byte) bool {
 	for _, b := range g {
 		if *b == val {
 			return true
@@ -101,19 +101,19 @@ func (g *group) has(val byte) bool {
 	return false
 }
 
-func contains(row, column, square *group, val byte) bool {
+func contains(row, column, square *Group, val byte) bool {
 	return row.has(val) || column.has(val) || square.has(val)
 }
 
-func Possibles(cell *byte, row, column, square *group) (b []byte) {
-	for _, i := range toSolve {
+func (p *Puzzle) Possibles(cell *byte, row, column, square *Group) (b []byte) {
+	for _, i := range p.ToSolve {
 		if !contains(row, column, square, i) {
 			b = append(b, i)
 		}
 	}
 
 	if len(b) == 1 {
-		solvedCell(cell, b[0])
+		p.solvedCell(cell, b[0])
 		return nil
 	}
 	return
